44cb070a9ed40961ecbdbd6956dcd787f2543c1e
eng-akram2003 <eng.akramalnowirah@gmail.com>
2025-09-19 21:34:47 +0300

    clsEmployee

---
+++ clsEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jira_Example
+{
+    internal class clsEmployee
+    {
+        public string Name { get; set; }
+        public decimal BasicSalary { get; set; }
+        public decimal Allowcess { get; set; }
+        public decimal Deductions { get; set; }
+
+        public clsEmployee(string Name, decimal BasicSalary, decimal Allowcess, decimal Deductions)
+        {
+            this.Name = Name;
+            this.BasicSalary = BasicSalary;
+            this.Allowcess = Allowcess;
+            this.Deductions = Deductions;
+        }
+
+        public decimal ClucluteSalary()
+        {
+            if (BasicSalary < 0 || Allowcess < 0 || Deductions < 0)
+                Console.WriteLine("value cannot be negtave");
+
+
+            return BasicSalary + Allowcess - Deductions;
+        }
+
+    }
+
+}
